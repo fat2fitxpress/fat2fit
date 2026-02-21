28c4e412e1a7bab0ef79652e2b37e2a18007a3e2
Fat2Fit Developer <dev@fat2fitxpress.com>
2026-02-21 13:00:37 -0500

    fix: replace next/script GTM with plain script tag for static export compatibility

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import type { Metadata, Viewport } from 'next';
-import Script from 'next/script';
 import ThemeRegistry from '@/theme/ThemeRegistry';
 import Navbar from '@/components/Navbar';
 import Footer from '@/components/Footer';
@@ -89,24 +88,8 @@ export default function RootLayout({
   const gtmId = process.env.NEXT_PUBLIC_GTM_ID || '';
   return (
     <html lang="en">
-      <head>
-        {/* Google Tag Manager */}
-        {gtmId && (
-          <Script
-            id="gtm-script"
-            strategy="afterInteractive"
-            dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${gtmId}');`,
-            }}
-          />
-        )}
-      </head>
       <body>
-        {/* Google Tag Manager (noscript) */}
+        {/* Google Tag Manager (noscript fallback) */}
         {gtmId && (
           <noscript>
             <iframe
@@ -116,6 +99,18 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
               style={{ display: 'none', visibility: 'hidden' }}
             />
           </noscript>
+        )}
+        {/* Google Tag Manager */}
+        {gtmId && (
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');`,
+            }}
+          />
         )}
         <ThemeRegistry>
           <ErrorBoundary>
